0c364c7635a66b5dbde786a7d8e2bfc9ac8caed7
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-19 09:48:02 -0300

    UPDATE

---
+++ ContaBancaria/src/ContaTerminal.java
@@ -0,0 +1,9 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        // TODO: Conhecer e importar a classe Scanner
+        // Exibir mensagem para nosso usuario
+        // Obter pelo scanner os valores digitados no terminal
+        // exibir a mensagem da conta criada
+    }
+  }
+  
